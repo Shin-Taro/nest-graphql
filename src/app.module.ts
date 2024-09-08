baad82a9b081bc3f884b23b0afb265e8ab1cd5de
Shin-Taro <sh.takahashi.113@gmail.com>
2024-09-08 18:37:40 +0900

    feat: generate posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaService } from './prisma.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './modules/users/users.module';
       autoSchemaFile: './src/schema.graphql',
     }),
     UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
